9adf95bf03d1f08662d5d5cdd0cddf5bb8490f99
olgaolsh <oliprogram@gmail.com>
2024-09-08 21:05:33 +0300

    6 cha

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Яблоки', price: 5000, description: 'Вкусные'},
+    {id: '2', title: 'Апельсины', price: 12000, description: 'Спелые'},
+    {id: '3', title: 'Персики', price: 5000, description: 'Вкусные'},
+    {id: '4', title: 'Лимоны', price: 122, description: 'Вкусные'},
+    {id: '5', title: 'Манго', price: 5000, description: 'Спелое'},
+    {id: '6', title: 'Вишня', price: 600, description: 'Спелая'},
+    {id: '7', title: 'Бананы', price: 5500, description: 'Вкусные'},
+    {id: '8', title: 'Ананас', price: 12000, description: 'Вкусный из Африки'},
 ]
 
 const getTotalPrice = (items = []) => {
