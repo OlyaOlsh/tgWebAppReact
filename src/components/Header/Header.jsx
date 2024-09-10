5141177b29f0606fd90566882bebf70c629c27e4
olgaolsh <oliprogram@gmail.com>
2024-09-11 00:18:47 +0300

    14chang

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Button from  "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css';
+import headerImage  from "./../../imagesItem/headerImage.png";
 
  const Header = () => {
    const{user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
+              <img src={headerImage} alt="Header Background" className='header-image' />
            {/* <Button onClick ={onClose}>Закрыть</Button>*/}
             <span className={'username'}>
              Добро пожаловать {user?.username}!
