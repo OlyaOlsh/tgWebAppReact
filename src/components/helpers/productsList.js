02e0576dfbaa39a28789d9380ef13f64af1ad9a6
olgaolsh <oliprogram@gmail.com>
2024-09-16 18:07:14 +0300

    20cha

--- src/components/helpers/productsList.js
+++ src/components/helpers/productsList.js
@@ -16,15 +16,19 @@ import curImageBig6  from "./../../imagesItem/imageBig6.jpg";
 import curImageBig7  from "./../../imagesItem/imageBig7.jpg";
 import curImageBig8  from "./../../imagesItem/imageBig8.jpg";
 
+import curAppleBig1 from "./../../imagesItem/AppleBig1.png";
+import curAppleBig2 from "./../../imagesItem/AppleBig2.png";
 
 const products = [
-    {id: '1', title: 'Яблоки', price: 5000, description: 'Вкусные', img: curImage1, imgBig: curImageBig1},
-    {id: '2', title: 'Апельсины', price: 1200, description: 'Спелые', img: curImage2, imgBig: curImageBig2},
-    {id: '3', title: 'Персики', price: 5000, description: 'Вкусные', img: curImage3, imgBig: curImageBig3},
-    {id: '4', title: 'Лимоны', price: 122, description: 'Вкусные', img: curImage4, imgBig: curImageBig4},
-    {id: '5', title: 'Манго', price: 5000, description: 'Спелое', img: curImage5, imgBig: curImageBig5},
-    {id: '6', title: 'Вишня', price: 600, description: 'Спелая', img: curImage6, imgBig: curImageBig6},
-    {id: '7', title: 'Бананы', price: 5500, description: 'Вкусные', img: curImage7, imgBig: curImageBig7},
-    {id: '8', title: 'Ананас', price: 1200, description: 'Вкусный', img: curImage8, imgBig: curImageBig8},
+    {id: '1', title: 'Яблоки', price: 5000, description: 'Вкусные', img: curImage1, imgBig: curImageBig1, images: [curAppleBig1, curAppleBig2,curImageBig1]},
+    {id: '2', title: 'Апельсины', price: 1200, description: 'Спелые', img: curImage2, imgBig: curImageBig2, images: [curImageBig2, curImageBig2, curImageBig2, curImageBig2]},
+    {id: '3', title: 'Персики', price: 5000, description: 'Вкусные', img: curImage3, imgBig: curImageBig3, images: [curImageBig3, curImageBig3]},
+    {id: '4', title: 'Лимоны', price: 122, description: 'Вкусные', img: curImage4, imgBig: curImageBig4, images: [curImageBig4, curImageBig4]},
+    {id: '5', title: 'Манго', price: 5000, description: 'Спелое', img: curImage5, imgBig: curImageBig5, images: [curImageBig5, curImageBig5]},
+    {id: '6', title: 'Вишня', price: 600, description: 'Спелая', img: curImage6, imgBig: curImageBig6, images:[curImageBig6, curImageBig6]},
+    {id: '7', title: 'Бананы', price: 5500, description: 'Вкусные', img: curImage7, imgBig: curImageBig7, images: [curImageBig7, curImageBig7]},
+    {id: '8', title: 'Ананас', price: 1200, description: 'Вкусный', img: curImage8, imgBig: curImageBig8, images: [curImageBig8, curImageBig8]},
 ]
+
+
 export {products}
--- src/pages/ItemPage/ItemPage.jsx
+++ src/pages/ItemPage/ItemPage.jsx
@@ -2,31 +2,44 @@ import React from 'react';
 
 import "./../ItemPage/ItemPage.css";
 import { useParams } from 'react-router-dom';
-import  "./../../components/ProductList/ProductList";
-{/*import {ProductList} from "./../../ProductList/ProductList"; */}
-{/*import ProductItem from "./../../components/ProductItem/ProductItem"; */}
+{/*import  "./../../components/ProductList/ProductList";*/}
 
-import { products } from "./../../components/helpers/productsList"
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-
-{/*import img  from "./../../imagesItem/imageBig1.jpg";*/}
+import { products } from "./../../components/helpers/productsList";
 
 const ItemPage = () => {
 const {id} = useParams();
 const productCur = products[id];
 
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
+
 
     return ( <main className="section">
         <div className="container">
             <div className="project-details">
-{console.log(productCur)};
+
                 <h1 className="title-1">{productCur.title}</h1>
 
-                <img src={productCur.imgBig} alt={productCur.title} className="project-details__cover"/>
+                <Slider {...settings}>
+                        {productCur.images.map((image, index) => (
+                            <div key={index}>
+                                <img src={image} alt={productCur.title} className="project-details__cover" />
+                            </div>
+                        ))}
+                </Slider>
 
                 <div className="project-details__desc">
                     <p>{productCur.description}</p>
-                </div>
+                </div> 
 
             </div>
         </div>
