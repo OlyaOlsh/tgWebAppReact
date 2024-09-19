0d349436ace0a825c5f724fd88dff307396f6e2c
olgaolsh <oliprogram@gmail.com>
2024-09-19 12:48:16 +0300

    21p

--- api/datebase.js
+++ api/datebase.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./products.db');
+const db = new sqlite3.Database('./api/products.db');
 
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, price REAL, image TEXT)", (err) => {
